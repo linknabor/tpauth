3e555097b5027976503cdd4995f4de00551cdeb9
davidhardson <davidhardson@hotmail.com>
2021-03-12 14:50:14 +0800

    fix

--- src/main/java/com/eshequ/hexie/tpauth/web/WechatMessageController.java
+++ src/main/java/com/eshequ/hexie/tpauth/web/WechatMessageController.java
@@ -1,24 +1,21 @@
 package com.eshequ.hexie.tpauth.web;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.eshequ.hexie.tpauth.service.WechatMessageService;
 import com.eshequ.hexie.tpauth.vo.EventRequest;
 
-@Controller
+@RestController
 public class WechatMessageController {
 	
 	private Logger logger = LoggerFactory.getLogger(WechatMessageController.class);
